f3c3142ef641b928aa9bf1eea698fb1e69fb086c
octavius-1993 <aef39@cam.ac.uk>
2016-08-22 19:17:29 +0100

    Added initial views

--- sploodl/expenses/views.py
+++ sploodl/expenses/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the Sploodl index.")
 
+def listView(request, uuid):
+    return HttpResponse("There are %d transactions recorded in %s" % (1, uuid))
+
