19c0919df8ff1db78a82b60e199513786aab520c
octavius-1993 <aef39@cam.ac.uk>
2016-08-22 16:33:11 +0100

    Update settings to use postgreSQL

--- sploodl/sploodl/settings.py
+++ sploodl/sploodl/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'sploodl.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db.postgres',
+        'USER': 'django',
+        'PASSWORD': 'tim=bean',
+        'HOST': '127.0.0.1'
     }
 }
 
@@ -103,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-uk'
 
 TIME_ZONE = 'UTC'
 
