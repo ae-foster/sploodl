956bab724ac343a06c83686f04bf1901123c9336
octavius-1993 <aef39@cam.ac.uk>
2016-08-22 18:23:52 +0100

    Further updates to models, including add date to all transactions

--- sploodl/expenses/models.py
+++ sploodl/expenses/models.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals
+import uuid
 
 from django.db import models
+from django.utils import timezone
 
 CURRENCIES = [('GBP', 'British Pound Sterling'),
                 ('USD', 'United States Dollar'),
@@ -10,19 +12,30 @@ CURRENCIES = [('GBP', 'British Pound Sterling'),
 
 
 class Sploodl(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=140)
     home_currency = models.CharField(max_length=3, choices = CURRENCIES)
 
+    def __str__(self):
+        return self.name
 
 
 class Participant(models.Model):
     sploodl = models.ForeignKey(Sploodl, on_delete=models.CASCADE)
     name = models.CharField(max_length=70)
 
+    def __str__(self):
+        return self.name
+
 class Transaction(models.Model):
     sploodl = models.ForeignKey(Sploodl, on_delete=models.CASCADE)
+    date = models.DateTimeField(default = timezone.now)
     description = models.CharField(max_length=140)
     currency = models.CharField(max_length=3, choices = CURRENCIES)
     people_by = models.ManyToManyField(Participant, related_name='+')
     people_for = models.ManyToManyField(Participant, related_name='+')
     value = models.DecimalField(max_digits=12, decimal_places=2)
+
+    def __str__(self):
+        return self.description
+
